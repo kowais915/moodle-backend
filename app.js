18215c489958c731773d541aa82240464090b9fb
kowais915 <kowais915@gmail.com>
2023-08-06 15:23:13 +0500

    :card_file_box: Connected to Mongodb

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3002;
+require('dotenv').config();
 
 const app = express();
 
+app.use(express.json());
 
-app.listen(3001, ()=>{
-  console.log("listening on port 3001");
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>{
+  app.listen(3001, ()=>{
+    console.log("Connection established");
+    console.log("listening on port 3001");
+  })
+  
+})
+.catch((error)=>{
+  console.log("Could not connect to MongoDB", error); 
 })
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res)=>
   res.status(200).json({
     msg: "Welcome to the Moodle API"
   })
-})
+)
